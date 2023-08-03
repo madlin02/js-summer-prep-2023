26700133b52ae3458828e41c37b3e2d376d36b5f
madlin02 <103335493+madlin02@users.noreply.github.com>
2023-08-03 13:25:37 -0400

    Update validators.js

--- session-01/validators.js
+++ session-01/validators.js
@@ -7,7 +7,22 @@
   - username cannot contain special characters
 */
 function validUsername(username) {
-  return;
+const allLetters= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const specialCharacters= "(){}[]|`¬¦! £$%^&*<>:;#~_-+=,@"
+const containsSpecialCharacters = new RegExp(`[${specialCharacters}]`).test(username);
+const containsNumbers= /^\d+$/.test(username)
+
+  if (
+    username.length >= 3 && 
+    username.length <= 10 && 
+    allLetters.includes(username[0]) && 
+    !containsSpecialCharacters || 
+    containsNumbers== true
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /*
@@ -17,7 +32,22 @@ function validUsername(username) {
   - password must contain at least 1 letter, 1 number, and 1 special character
 */
 function validPassword(password) {
-  return;
-}
+const allLetters= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const containsNumbers= /^\d+$/.test(password)
+const specialCharacters= "(){}[]|`¬¦! £$%^&*<>:;#~_-+=,@"
+const containsSpecialCharacters = new RegExp(`[${specialCharacters}]`).test(password);
 
+
+  if (
+    password.length >= 10 && 
+    password.length <= 64 && 
+    allLetters.includes(password) &&
+    containsNumbers== true &&
+    containsSpecialCharacters== true
+ ) {
+    return true;
+  } else {
+    return false;
+  }
+}
 module.exports = { validUsername, validPassword };
